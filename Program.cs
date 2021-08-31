877ac553150bacd5d05ac78e9af8c92406c146e9
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-08-31 18:37:13 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.CommandLine;
+
+namespace wfpm_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("開発開始すぐすぎるのでとりましっぽりをダウンロードするだけ");
+            WebClient wb = new WebClient();
+            wb.DownloadFile("https://fontdasu.com/download/shippori3.zip","/tmp/shippori3.zip");
+        }
+    }
+}
